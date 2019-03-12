38b2cddefc16db16a4b19c2a208baf44558a22f8
Jeferson <jefersonaraujo95@gmail.com>
2019-03-12 17:42:43 -0300

    lets go

--- conexao.py
+++ conexao.py
@@ -1,3 +1,9 @@
 from zabbix_api import ZabbixAPI
 
 URL = 'http://zabbix.fortalnet.net.br/zabbix/api_jsonrpc.php'
+USERNAME = 'zabbix'
+PASSWORD = ''
+
+zapi = ZabbixAPI(URL, timeout=180)
+zapi.login(USERNAME, PASSWORD)
+print('Conectado na API do Zabbix, versao: {}'.format(zapi.api_version()))
