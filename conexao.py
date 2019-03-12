c21fe6e07260ee4e29b31f9bc79ea12be6644f19
Jeferson <jefersonaraujo95@gmail.com>
2019-03-12 17:45:11 -0300

    lets go

--- conexao.py
+++ conexao.py
@@ -4,6 +4,12 @@ URL = 'http://zabbix.fortalnet.net.br/zabbix/api_jsonrpc.php'
 USERNAME = 'zabbix'
 PASSWORD = ''
 
-zapi = ZabbixAPI(URL, timeout=180)
-zapi.login(USERNAME, PASSWORD)
-print('Conectado na API do Zabbix, versao: {}'.format(zapi.api_version()))
+try:
+	zapi = ZabbixAPI(URL, timeout=180)
+	zapi.login(USERNAME, PASSWORD)
+	print('Conectado na API do Zabbix, versao: {}'.format(zapi.api_version()))
+
+except Exception as err:
+	print('Falha ao Conectar na API no Zabbix')
+	print('Erro: {}'.format(err))
+	
