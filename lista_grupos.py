1bd97ab62379975230a1c503e2ece1b73f12c4d1
Jeferson <jefersonaraujo95@gmail.com>
2019-03-14 17:03:00 -0300

    lets go

---
+++ lista_grupos.py
@@ -0,0 +1,28 @@
+from zabbix_api import ZabbixAPI
+
+URL = 'http://zabbix.fortalnet.net.br/zabbix/api_jsonrpc.php'
+USERNAME = 'zabbix'
+PASSWORD = ''
+
+try:
+	zapi = ZabbixAPI(URL, timeout=180)
+	zapi.login(USERNAME, PASSWORD)
+	print('Conectado na API do Zabbix, versao: {}'.format(zapi.api_version()))
+
+
+
+except Exception as err:
+	print('Falha ao Conectar na API no Zabbix')
+	print('Erro: {}'.format(err))
+
+
+hosts = zapi.host.get({
+    "output": ['host', 'name', 'status']
+})
+
+for host in hosts:
+    host_id = host['hostid']
+    host_name = host['host']
+    host_visiblename = host['name']
+    host_status = host['status']
+    print('{} - {} - {} - {}'.format(host_id, host_name, host_visiblename, host_status))
