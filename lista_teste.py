5e4671053bdf2de548ad5494725b0b887d418abd
Jeferson <jefersonaraujo95@gmail.com>
2019-03-15 09:54:01 -0300

    lets go

--- lista_teste.py
+++ lista_teste.py
@@ -15,20 +15,28 @@ except Exception as err:
 	print('Falha ao Conectar na API no Zabbix')
 	print('Erro: {}'.format(err))
 
+def filtrando_hosts(nome_host):
+    hosts = zapi.host.get({
+        "output": ['host', 'name', 'status'],
+        "filter": {"host": nome_host}
+    })
+    
+    
+    if hosts:
+    	for host in hosts:
+    		host_id = host['hostid']
+    		host_name = host['host']
+    		host_visiblename = host['name']
+    		host_status = host['status']
+    		print('{} - {} - {} - {}'.format(host_id, host_name, host_visiblename, host_status))	
+    
+    else:
+        print('Host não encontrado')
 
-hosts = zapi.host.get({
-    "output": ['host', 'name', 'status'],
-    "filter": {"host": 'machine1'}
-})
 
 
-if hosts:
-	for host in hosts:
-		host_id = host['hostid']
-		host_name = host['host']
-		host_visiblename = host['name']
-		host_status = host['status']
-		print('{} - {} - {} - {}'.format(host_id, host_name, host_visiblename, host_status))	
+nome_do_host = input('Digite o nome do host: ')
+filtrando_hosts(nome_do_host)
 
-else:
-    print('Host não encontrado')
+
+
