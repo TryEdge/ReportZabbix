5211348642212b7eb6ca74434d85dbdb96d23352
Jeferson Araujo <jefersonaraujo95@gmail.com>
2020-03-19 16:41:00 -0300

    export csv file now

--- Lista_hosts.py
+++ Lista_hosts.py
@@ -1,9 +1,12 @@
+#coding:utf-8
+import csv
 from zabbix_api import ZabbixAPI
-
+from datetime import date
 URL = 'http://zabbix/zabbix/api_jsonrpc.php'
 USERNAME = 'user_zabbix'
-PASSWORD = 'user_pass'
-
+PASSWORD = 'user_zabbix_pass'
+data_atual = date.today().strftime('%d-%m-%Y')
+header=["ID","HOST","NOME VISIVEL","STATUS"]
 try:
 	zapi = ZabbixAPI(URL, timeout=180)
 	zapi.login(USERNAME, PASSWORD)
@@ -38,3 +41,11 @@ if hosts:
 		host_visiblename = host['name']
 		host_status = host['status']
 		print('{} - {} - {} - {}'.format(host_id, host_name, host_visiblename, host_status))
+
+
+with open('zabbix_list_host-'+data_atual+'.csv', mode='w') as file:
+   output = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+   output.writerow(header)
+   for host in hosts:
+      row=[host['hostid'],host['host'],host['name'],host['status']]
+      output.writerow(row)
