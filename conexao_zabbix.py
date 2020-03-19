102f32ec48712d87d63ce30c2af31e658374e93e
Jeferson Araujo <jefersonaraujo95@gmail.com>
2020-03-19 11:16:30 -0300

    Add conexao

---
+++ conexao_zabbix.py
@@ -0,0 +1,15 @@
+from zabbix_api import ZabbixAPI
+
+URL = 'http://meuzabbix_url/zabbix/api_jsonrpc.php'
+USERNAME = 'zabbix'
+PASSWORD = ''
+
+try:
+	zapi = ZabbixAPI(URL, timeout=180)
+	zapi.login(USERNAME, PASSWORD)
+	print('Conectado na API do Zabbix, versao: {}'.format(zapi.api_version()))
+
+except Exception as err:
+	print('Falha ao Conectar na API no Zabbix')
+	print('Erro: {}'.format(err))
+
